ac236a81c40a33695634f04c493e1cf6d715a74e
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-01 13:56:16 +0100

    colorclick fini à 100%

--- reacttypescript-exos/react-typescript-exos/src/ColorClick.tsx
+++ reacttypescript-exos/react-typescript-exos/src/ColorClick.tsx
@@ -10,32 +10,15 @@ import { useState } from 'react';
 export default function ColorClick() {
     const couleurs = ["red", "green", "blue", "wheat"];
 
-    const [color, setColor] = useState(couleurs[3
-
-    ]
-
-    );
+    const [color, setColor] = useState(couleurs[3]);
 
 
 
-    // function handleClick() {
-    //     setColor([
-    //         "red",
-    //         "green",
-    //         "blue"
-    //     ]
-    //     );i
+    const coulours = ["white", "black", "red"];
+    const [textColor, setTextColor] = useState(coulours[1]);
 
 
-    // }
-
-    // const [textColor, setTextColor] = useState("green");
-    // function handleClicktextcolor() {
-
-    //     setTextColor("red");
-
-
-    // }
+  
 
     return (
         <>
@@ -46,15 +29,15 @@ export default function ColorClick() {
                     <button onClick={() => setColor(couleurs[0])} className="red" >Red</button>
                     <button onClick={() => setColor(couleurs[1])} className="green">Green</button>
                     <button onClick={() => setColor(couleurs[2])} className="blue">Blue</button>
-                    {/* 
-                    <h3>textColor</h3>
-                    <button onClick={handleClicktextcolor} className='white' >White</button>
-                    <button onClick={handleClicktextcolor} className='black' >Black</button>
-                    <button onClick={handleClicktextcolor} className='red' >Red</button> */}
-                </div>
-                {/* la couleur doit apparaître dans boxedeux */}
 
-                <div className="boxedeux" style={{ backgroundColor: color }}>
+                    <h3>textColor</h3>
+                    <button onClick={() => setTextColor(coulours[0])} className='white' >White</button>
+                    <button onClick={() => setTextColor(coulours[1])} className='black' >Black</button>
+                    <button onClick={() => setTextColor(coulours[2])} className='red' >Red</button>
+                </div>
+
+
+                <div className="boxedeux" style={{ backgroundColor: color, color : textColor}}>
                     <h3>Oh rage, ô désespoir</h3>
                     <p>Oh rage, ô désespoir, que mon souffle se brise,
                         Quand le jour s'efface et que l'âme s'afflige.
