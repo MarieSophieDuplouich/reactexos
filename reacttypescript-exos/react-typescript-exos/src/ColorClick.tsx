39fd4e450f4a6a38c2926aee03073870a81b3a57
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-12-01 13:47:09 +0100

    colorclick fait à 50%

--- reacttypescript-exos/react-typescript-exos/src/ColorClick.tsx
+++ reacttypescript-exos/react-typescript-exos/src/ColorClick.tsx
@@ -1,53 +1,67 @@
 // style css attribut c'est quand on change les couleurs
 
 import { useState } from 'react';
+// type Couleur = {
+//     red: string;
+//     green: string;
+//     blue: string;
+// };
 
 export default function ColorClick() {
-    const [color, setColor] = useState("blue");
-  
+    const couleurs = ["red", "green", "blue", "wheat"];
 
-    function handleClick() {
-        setColor("pink");
-       
-    }
-    
-          const [textColor, setTextColor] = useState("green");
-    function handleClicktextcolor(){
-    
-                 setTextColor("red");
-    }
+    const [color, setColor] = useState(couleurs[3
+
+    ]
+
+    );
+
+
+
+    // function handleClick() {
+    //     setColor([
+    //         "red",
+    //         "green",
+    //         "blue"
+    //     ]
+    //     );i
+
+
+    // }
+
+    // const [textColor, setTextColor] = useState("green");
+    // function handleClicktextcolor() {
+
+    //     setTextColor("red");
+
+
+    // }
 
     return (
         <>
-    <button style={{ background: color, color:textColor }} onClick={handleClick}>
-      You pressed me {color} times 
-    </button>
+            <h2>ColorClick</h2>
+            <section className="click">
+                <div className="boxeun">
+                    <h3>backgroundColor</h3>
+                    <button onClick={() => setColor(couleurs[0])} className="red" >Red</button>
+                    <button onClick={() => setColor(couleurs[1])} className="green">Green</button>
+                    <button onClick={() => setColor(couleurs[2])} className="blue">Blue</button>
+                    {/* 
+                    <h3>textColor</h3>
+                    <button onClick={handleClicktextcolor} className='white' >White</button>
+                    <button onClick={handleClicktextcolor} className='black' >Black</button>
+                    <button onClick={handleClicktextcolor} className='red' >Red</button> */}
+                </div>
+                {/* la couleur doit apparaître dans boxedeux */}
 
+                <div className="boxedeux" style={{ backgroundColor: color }}>
+                    <h3>Oh rage, ô désespoir</h3>
+                    <p>Oh rage, ô désespoir, que mon souffle se brise,
+                        Quand le jour s'efface et que l'âme s'afflige.
+                        Je jette mes mots contre le mur du silence, Espérant qu'un écho rende enfin ma sentence.</p>
+                </div>
 
-
-
-    <h2>ColorClick</h2>
-    <section className="click">
-    <div className="boxeun">
-        <h3>backgroundColor</h3>
-        <button onClick={handleClick} className='red' >Red</button>
-        <button  onClick={handleClick} className='green'>Green</button>
-        <button  onClick={handleClick} className='blue'>Blue</button>
-
-        <h3>textColor</h3>
-         <button  onClick={ handleClicktextcolor} className='white' >White</button>
-        <button onClick={ handleClicktextcolor}className='black' >Black</button>
-        <button  onClick={ handleClicktextcolor}className='red' >Red</button>
-    </div>
-    
-    <div className="boxedeux" style={{ background: color, color:textColor }}>
-    <h3>Oh rage, ô désespoir</h3>
-    <p>Oh rage, ô désespoir, que mon souffle se brise,
-         Quand le jour s'efface et que l'âme s'afflige. 
-         Je jette mes mots contre le mur du silence, Espérant qu'un écho rende enfin ma sentence.</p>
-    </div>
-
-    </section>
-    </>
+            </section>
+        </>
     );
 }
